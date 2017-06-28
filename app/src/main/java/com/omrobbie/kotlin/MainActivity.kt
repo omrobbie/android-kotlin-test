5a77c4812c85465c4c03a7e8896b42a7edbfdc04
omrobbie <putu.roby@yahoo.com>
2017-06-28 11:34:16 +0800

    coba menggunakan kotlin

---
+++ app/src/main/java/com/omrobbie/kotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.omrobbie.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setText("text ini di ubah dengan kotlin")
+        button.setOnClickListener {
+            Toast.makeText(this, "toast ini muncul dengan kotlin", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
